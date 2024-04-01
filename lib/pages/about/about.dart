8201374fa73d429f6ed723b012118889551c0c9d
hutao <332413594@qq.com>
2024-04-01 09:17:34 +0800

    feat: 我的页面

--- lib/pages/about/about.dart
+++ lib/pages/about/about.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../utils/storage/storage.dart';
 
 /// 关于我
 class AboutWidget extends StatefulWidget {
@@ -10,22 +9,80 @@ class AboutWidget extends StatefulWidget {
 }
 
 class _AboutWidgetState extends State<AboutWidget> {
-  final storage = Storage();
 
-  void _getStore() async {
-    var s = await storage.getKeys();
-    print(s);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('demo'),
-      ),
-      body: ElevatedButton(
-        onPressed: _getStore,
-        child: const Text('获取'),
+      backgroundColor: Colors.black12,
+      body: Column(
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text('张三'),
+            accountEmail: Text('zhangsan@goole.com'),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/image1.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage('https://qiniu.public.hutaoao.cn/invitation/1.jpg'),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(0),
+              children: const [
+                ListTile(
+                  title: Text('应用管理'),
+                  leading: Icon(Icons.window),
+                  trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                  tileColor: Colors.white,
+                ),
+                Divider(height: 1),
+                ListTile(
+                  title: Text('数据管理'),
+                  leading: Icon(Icons.data_exploration),
+                  trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                  tileColor: Colors.white,
+                ),
+                Divider(height: 1),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: ListTile(
+                    title: Text('我的收藏'),
+                    leading: Icon(Icons.collections),
+                    trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                    tileColor: Colors.white,
+                  ),
+                ),
+                ListTile(
+                  title: Text('版本信息'),
+                  leading: Icon(Icons.verified_sharp),
+                  trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                  tileColor: Colors.white,
+                ),
+                Divider(height: 1),
+                ListTile(
+                  title: Text('关于应用'),
+                  leading: Icon(Icons.sailing),
+                  trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                  tileColor: Colors.white,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: ListTile(
+                    title: Text('联系我'),
+                    leading: Icon(Icons.contacts),
+                    trailing: Icon(Icons.keyboard_arrow_right_outlined, color: Colors.red),
+                    tileColor: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
