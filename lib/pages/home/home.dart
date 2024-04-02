c4ab314ffce93ecbee39c08555d9b919c7550dfc
hutao <332413594@qq.com>
2024-04-02 16:07:28 +0800

    feat: 首页内容补充

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -11,13 +11,101 @@ class HomeWidget extends StatelessWidget {
   HomeWidget({super.key});
 
   final storage = Storage();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Widget _buildDrawer() => ListView(
+    children: const [
+      DrawerHeader(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.only(left: 30, top: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40)
+          ),
+          image: DecorationImage(
+            image: AssetImage('assets/images/image1.jpg'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Text('hello flutter', style: TextStyle(color: Colors.white, fontSize: 20)),
+      ),
+      ListTile(
+        leading: Icon(Icons.star, color: Colors.blue),
+        title: Text('我的收藏'),
+      ),
+      ListTile(
+        leading: Icon(Icons.palette, color: Colors.orangeAccent),
+        title: Text('我的绘画'),
+      ),
+      ListTile(
+        leading: Icon(Icons.file_copy, color: Colors.green),
+        title: Text('我的文件'),
+      )
+    ],
+  );
+
+  final Map<String, IconData> menuMap = {
+    '关于': Icons.info_outline,
+    '帮助': Icons.help_outline,
+    '反馈': Icons.add_comment,
+  };
+
+  Widget _buildMenu() => PopupMenuButton(
+    itemBuilder: (context) => _buildPopupMenuItem(),
+    offset: const Offset(0, 50),
+    icon: const Icon(Icons.more_vert, color: Colors.teal),
+    iconSize: 30,
+  );
+
+  List<PopupMenuItem> _buildPopupMenuItem() {
+    return menuMap.keys.toList().map((e) => PopupMenuItem(
+      value: e,
+      child: Wrap(
+        spacing: 10,
+        children: [
+          Icon(menuMap[e], color: Colors.blue),
+          Text(e)
+        ],
+      ),
+    )).toList();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     ScreenUtil().setSp(28);
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: const Text('home'),
+        title: const Text('首页'),
+        backgroundColor: Colors.amber[100],
+        leading: Center(
+          child: GestureDetector(
+            onTap: () => _scaffoldKey.currentState?.openDrawer(),
+            child: ClipOval(
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 36,
+                height: 36,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () => _scaffoldKey.currentState?.openEndDrawer(),
+            child: const Icon(Icons.star, size: 36,  color: Colors.red),
+          ),
+          _buildMenu()
+        ],
+      ),
+      drawer: Drawer(
+        child: _buildDrawer(),
+      ),
+      endDrawer: Drawer(
+        child: _buildDrawer(),
       ),
       body: Column(
         children: [
